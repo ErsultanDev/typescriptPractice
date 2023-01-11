d682cbcc8c8d6f7491638eeba8f84ec7145d32b7
Ersultan <ersultansansyzbaj012@gmail.com>
2023-01-11 21:33:31 +0600

    first commit

---
+++ index.ts
@@ -0,0 +1,24 @@
+import axios from "axios";
+const url = "https://jsonplaceholder.typicode.com/todos/1";
+
+interface Todo {
+  id: number;
+  title: string;
+  completed: boolean;
+}
+
+axios.get(url).then((response) => {
+  const todo = response.data as Todo;
+
+  const id = todo.id;
+  const title = todo.title;
+  const finished = todo.completed;
+  logTodo(id, finished, title);
+});
+function logTodo(id, title, completed) {
+  console.log(`
+Todo ID : ${id}
+Todo title : ${title}
+Todo finished : ${completed}
+`);
+}
